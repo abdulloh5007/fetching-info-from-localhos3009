7dd0df35fdf891b0339c11786b41060d17144c76
abdulloh5007 <abdulloh50007@gmail.com>
2023-08-25 19:25:11 +0500

    new

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import style from '../styles/globals.module.scss'
+import Link from 'next/link';
 
 const Layout = ({ children }) => {
   return (
     <div className={style.container}>
       <header>
         <b>Header asd</b>
+        <Link href='/login'>Login</Link>
       </header>
       <main>{children}</main>
       <footer>
